6a8db408a8da8ee45cfc1ed5c7b477099cf49b9a
AJIN_VR <ajinvr44@gmail.com>
2024-07-28 22:50:01 +0530

    v2

--- client/src/pages/ContactForm.jsx
+++ client/src/pages/ContactForm.jsx
@@ -8,20 +8,20 @@ const errorStyle = 'text-red-500 mt-1';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    source: ''
+    "entry.1517675444": '',
+    "entry.1277496823": '',
+    "entry.408859639": '',
+    "entry.733189515": ''
   });
 
   const [errors, setErrors] = useState({
-    name: false,
-    email: false,
-    phone: false,
-    source: false
+    "entry.1517675444": false,
+    "entry.1277496823": false,
+    "entry.408859639": false,
+    "entry.733189515": false
   });
 
-  const redirectLink = 'https://chat.whatsapp.com/DNZMRvg38ay8nNwG3VAegu';
+  const redirectLink = 'https://chat.whatsapp.com/DNZMRvg38ay8nNwG3VAegu'; 
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,17 +39,19 @@ const ContactForm = () => {
     e.preventDefault();
     const isValid = validateForm();
     if (isValid) {
-      redirectToLink();
+      document.getElementById('contactForm').submit();
+      setTimeout(() => {
+        window.location.href = redirectLink;
+      }, 200);
     }
   };
 
   const validateForm = () => {
-    const { name, email, phone, source } = formData;
     const newErrors = {
-      name: !name,
-      email: !email || !validateEmail(email),
-      phone: !phone,
-      source: !source
+      "entry.1517675444": !formData["entry.1517675444"],
+      "entry.1277496823": !formData["entry.1277496823"] || !validateEmail(formData["entry.1277496823"]),
+      "entry.408859639": !formData["entry.408859639"],
+      "entry.733189515": !formData["entry.733189515"]
     };
     setErrors(newErrors);
     return !Object.values(newErrors).some(error => error);
@@ -60,70 +62,66 @@ const ContactForm = () => {
     return re.test(email);
   };
 
-  const redirectToLink = () => {
-    window.location.href = redirectLink;
-  };
-
   return (
     <div className='h-screen w-screen flex items-center justify-center bg-gradient-to-r from-blue-900 to-purple-900'>
       <div className='w-full max-w-md'>
         <FormHeadingText />
         <div className="mx-auto my-5 p-6 sm:border sm:rounded-lg sm:shadow-lg sm:bg-opacity-20 sm:backdrop-filter sm:backdrop-blur-lg">
-          <form onSubmit={handleSubmit}>
-            <div className={` ${errors.name ? 'mb-1' : 'mb-4'}`}>
-              <label htmlFor="name" className={labelstyle}>Name</label>
+          <form id="contactForm" action='https://docs.google.com/forms/d/e/1FAIpQLSeba0_vKRBo2ORM6hpld-IX5iVEsOrNf-PYkxwdS076fmT3zg/formResponse' method="POST" onSubmit={handleSubmit}>
+            <div className={` ${errors["entry.1517675444"] ? 'mb-1' : 'mb-4'}`}>
+              <label htmlFor="entry.1517675444" className={labelstyle}>Name</label>
               <input
                 type="text"
-                id="name"
-                name="name"
-                className={`${inputstyle} ${errors.name ? 'border-red-500' : ''}`}
+                id="entry.1517675444"
+                name="entry.1517675444"
+                className={`${inputstyle} ${errors["entry.1517675444"] ? 'border-red-500' : ''}`}
                 placeholder="Your name"
-                value={formData.name}
+                value={formData["entry.1517675444"]}
                 onChange={handleChange}
               />
-              {errors.name && <p className={errorStyle}>Name is required</p>}
+              {errors["entry.1517675444"] && <p className={errorStyle}>Name is required</p>}
             </div>
 
-            <div className={` ${errors.name ? 'mb-1' : 'mb-4'}`}>
-              <label htmlFor="email" className={labelstyle}>Email</label>
+            <div className={` ${errors["entry.1277496823"] ? 'mb-1' : 'mb-4'}`}>
+              <label htmlFor="entry.1277496823" className={labelstyle}>Email</label>
               <input
                 type="email"
-                id="email"
-                name="email"
-                className={`${inputstyle} ${errors.email ? 'border-red-500' : ''}`}
+                id="entry.1277496823"
+                name="entry.1277496823"
+                className={`${inputstyle} ${errors["entry.1277496823"] ? 'border-red-500' : ''}`}
                 placeholder="Your email"
-                value={formData.email}
+                value={formData["entry.1277496823"]}
                 onChange={handleChange}
               />
-              {errors.email && <p className={errorStyle}>Valid email is required</p>}
+              {errors["entry.1277496823"] && <p className={errorStyle}>Valid email is required</p>}
             </div>
 
-            <div className={` ${errors.name ? 'mb-1' : 'mb-4'}`}>
-              <label htmlFor="phone" className={labelstyle}>Phone Number</label>
+            <div className={` ${errors["entry.408859639"] ? 'mb-1' : 'mb-4'}`}>
+              <label htmlFor="entry.408859639" className={labelstyle}>Phone Number</label>
               <input
                 type="tel"
-                id="phone"
-                name="phone"
-                className={`${inputstyle} ${errors.phone ? 'border-red-500' : ''}`}
+                id="entry.408859639"
+                name="entry.408859639"
+                className={`${inputstyle} ${errors["entry.408859639"] ? 'border-red-500' : ''}`}
                 placeholder="Your phone number"
-                value={formData.phone}
+                value={formData["entry.408859639"]}
                 onChange={handleChange}
               />
-              {errors.phone && <p className={errorStyle}>Phone number is required</p>}
+              {errors["entry.408859639"] && <p className={errorStyle}>Phone number is required</p>}
             </div>
 
-            <div className={` ${errors.name ? 'mb-1' : 'mb-4'}`}>
-              <label htmlFor="source" className={labelstyle}>Where did you get this link?</label>
+            <div className={` ${errors["entry.733189515"] ? 'mb-1' : 'mb-4'}`}>
+              <label htmlFor="entry.733189515" className={labelstyle}>Where did you get this link?</label>
               <textarea
-                id="source"
-                name="source"
-                className={`${inputstyle} ${errors.source ? 'border-red-500' : ''}`}
+                id="entry.733189515"
+                name="entry.733189515"
+                className={`${inputstyle} ${errors["entry.733189515"] ? 'border-red-500' : ''}`}
                 placeholder="Tell us where you got this link"
                 rows="1"
-                value={formData.source}
+                value={formData["entry.733189515"]}
                 onChange={handleChange}
               />
-              {errors.source && <p className={errorStyle}>This field is required</p>}
+              {errors["entry.733189515"] && <p className={errorStyle}>This field is required</p>}
             </div>
 
             <div className="flex justify-center">
